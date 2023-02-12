911aa9df7ffc33464c7c6218a036a9fa6aeade6e
Pouya Jafarian <pouya.jafarian@varian.com>
2023-02-12 01:48:54 -0800

    changed some var play and recursion code

--- archive/recursion.py
+++ archive/recursion.py
@@ -1,6 +1,16 @@
 # This function reverses a string. Using recursion.
 import sys
 
+class ListNode:
+    def __init__(self, value, next) -> None:
+        self.value = value
+        self.next = next
+class TreeNode:
+    def __init__(self, value, left, right) -> None:
+        self.value = value
+        self.right = right
+        self.left = left
+
 class Solution:
 
     def __init__(self, height: int = 2) -> None:
@@ -16,6 +26,46 @@ class Solution:
             return all_outs
         all_outs = str(dec % 2) + all_outs
         return self.decToBinary(int(dec / 2), all_outs)
+
+    def reverseLinkedList(self, root: ListNode) -> ListNode:
+        if not root:
+            return None 
+        else: 
+            previous = root
+            root = root.next
+            root.next = previous
+            return self.reverseLinkedList(root.next)
+    
+    def mergeSortedLinkedLists(self, root_a: ListNode, root_b: ListNode) -> ListNode:
+        if not root_a:
+            return root_b
+        if not root_b:
+            return root_a
+        if root_a.value < root_b.value:
+            root_a.next = self.mergeSortedLinkedLists(root_a.next, root_b)
+            return root_a
+        elif root_a.value > root_b.value:
+            root_a.next = self.mergeSortedLinkedLists(root_a, root_b.next)
+            return root_b
+
+    def insertNodeInBST(self, root: TreeNode, insertion: TreeNode) -> TreeNode:
+        if not root:
+            return insertion
+        else:
+            if insertion.value < root.value:
+                return self.insertNodeInBST(root.left, insertion)
+            else:
+                return self.insertNodeInBST(root.right, insertion)
+    
+    def printLeafNodesBST(self, root: TreeNode) -> list(TreeNode):
+        results = []
+        if not root:
+            return None
+        curr = root.next
+        self.printLeafNodesBST(curr)
+        if root.left is None and root.right is None:
+            results.append(root)
+        
 
 if __name__ == "__main__":
 
--- archive/vars.py
+++ archive/vars.py
@@ -2,8 +2,8 @@ class Solution:
     def varPlay(self,my_list: list) -> None:
         
         print("In the function:                 ", id(my_list))
-        # my_li.extend([17,3,4])
-        # print("In the function after modif. :   ", id(my_li))
+        my_list.extend([1])
+        print("In the function after modif. :   ", id(my_list))
         my_list = my_list + [12,15,17]
         print("In the function after re-ass :   ", id(my_list))
         print(my_list)
@@ -11,6 +11,7 @@ class Solution:
 if __name__ == "__main__":
     my_solution = Solution()
     my_list = [7,122,199]
+    print(my_list)
     print("In the main call:                ", id(my_list))
     my_solution.varPlay(my_list)
     print("the orig. again:                 ", id(my_list))
